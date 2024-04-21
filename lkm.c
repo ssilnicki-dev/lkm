b05673db263e98c3d02a42a504cb68f092f16199
Stanislav Silnicki <64929236+ssilnicki-dev@users.noreply.github.com>
2024-04-22 01:28:46 +0300

    imitate realtime device read

--- lkm.c
+++ lkm.c
@@ -1,9 +1,12 @@
+#include <linux/delay.h>
 #include <linux/hrtimer.h>
 #include <linux/init.h>
 #include <linux/ktime.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/random.h>
+#include <linux/slab.h>
+#include <linux/workqueue.h>
 
 static long kt_period_param_us = USEC_PER_SEC;
 module_param(kt_period_param_us, long, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
@@ -12,9 +15,44 @@ static int kt_period_jitter_range_param_us = 100;
 module_param(kt_period_jitter_range_param_us, int,
              S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 MODULE_PARM_DESC(kt_period_jitter_range_us, "Timer jitter range, us");
+static int device_read_jitter_range_param_us = 25;
+module_param(device_read_jitter_range_param_us, int,
+             S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(device_read_jitter_range_param_us, "Device read jitter, us");
+#define DEVICE_DATASHEET_MINIMUM_READ_US (80)
 
 static ktime_t kt_period;
 static struct hrtimer kt;
+
+enum device_read_work_stage_t { IN_PROGRESS, DONE };
+struct device_read_work_t {
+  struct work_struct work;
+  atomic_long_t work_stage;
+};
+
+struct device_read_work_t device_read_work;
+
+static void __hardware_device_blocking_read(void) {
+  static u32 runtime_data[16];
+  unsigned long usleep_us = DEVICE_DATASHEET_MINIMUM_READ_US;
+  int i;
+
+  for (i = 0; i < (sizeof(runtime_data) / sizeof(*runtime_data)); i = i + 1) {
+    runtime_data[i] = get_random_u32();
+  }
+
+  if (likely(device_read_jitter_range_param_us > 0)) {
+    usleep_us += get_random_u32() % device_read_jitter_range_param_us;
+  }
+  usleep_range(usleep_us - 1, usleep_us + 1);
+}
+
+static void read_data_from_device(struct work_struct *work) {
+  struct device_read_work_t *wrapper;
+  __hardware_device_blocking_read();
+  wrapper = container_of(work, struct device_read_work_t, work);
+  atomic_long_set(&wrapper->work_stage, DONE);
+}
 
 static enum hrtimer_restart kt_callback(struct hrtimer *timer) {
   if (likely(kt_period_jitter_range_param_us > 0)) {
@@ -22,6 +60,10 @@ static enum hrtimer_restart kt_callback(struct hrtimer *timer) {
     u64 period_us = kt_period_param_us + jitter_lag_us;
     kt_period = ktime_set(period_us / USEC_PER_SEC,
                           (period_us % USEC_PER_SEC) * NSEC_PER_USEC);
+  }
+  if (likely(atomic_long_read(&device_read_work.work_stage) == DONE)) {
+    atomic_long_set(&device_read_work.work_stage, IN_PROGRESS);
+    schedule_work(&device_read_work.work);
   }
   hrtimer_forward_now(timer, kt_period);
   return HRTIMER_RESTART;
@@ -38,6 +80,9 @@ static inline void __lkm_init_timer(long period_us) {
 static int __init lkm_init(void) {
   pr_info("lkm loaded\n");
 
+  atomic_long_set(&device_read_work.work_stage, DONE);
+  INIT_WORK(&device_read_work.work, read_data_from_device);
+
   __lkm_init_timer(kt_period_param_us);
 
   return 0;
@@ -45,6 +90,7 @@ static int __init lkm_init(void) {
 
 static void __exit lkm_exit(void) {
   hrtimer_cancel(&kt);
+  cancel_work(&device_read_work.work);
 
   pr_info("lkm unloaded\n");
 }
